ad9986f3bad764d3d900bccd0b935a2d6fcaf2a0
Lucas Ferreira <lucasgf.contato@gmail.com>
2022-02-18 16:22:59 -0300

    Projeto Criado

---
+++ src/test/java/com/Dev_Luc/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Dev_Luc.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
