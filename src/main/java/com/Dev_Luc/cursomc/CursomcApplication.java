bd793232337e0f8cfee36bcc4560e2252f28ad4b
Lucas Ferreira <lucasgf.contato@gmail.com>
2022-02-19 03:13:27 -0300

    REST TESTADO!

--- src/main/java/com/Dev_Luc/cursomc/CursomcApplication.java
+++ src/main/java/com/Dev_Luc/cursomc/CursomcApplication.java
@@ -1,9 +1,13 @@
 package com.Dev_Luc.cursomc;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan("com.Dev_Luc.cursomc.resources")
 public class CursomcApplication {
 
 	public static void main(String[] args) {
